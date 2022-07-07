4ec95a5368e8d84a2f2f32eee63482169522f770
Văn Huỳnh <vanhuynh@Vans-MacBook-Pro.local>
2022-07-07 17:39:44 +0700

    First commit 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'));
 app.get('/', (req, res) => {
-  res.send('Hello World 2!')
+  res.send(`
+  <h1> HELLO WORLD </h1>
+  `)
 })
 
 app.listen(port, () => {
