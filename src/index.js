65f0417d0f709301bb7e3b3072cfa01a3a310bdc
Văn Huỳnh <vanhuynh@Vans-MacBook-Pro.local>
2022-07-09 00:17:36 +0700

    Add folder views, resources, public

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const express = require('express')
-var morgan = require('morgan')
+const path = require('path')
+const morgan = require('morgan')
+
+const handlebars = ('express-handlebars');
+const { engine } = require ('express-handlebars');
+
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('combined'));
+app.engine('hbs', engine({
+  extname: 'hbs',
+  // defaultLayout:false,
+  // layoutsDir:"resource/views"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'/resources/views'));
+
+console.log(__dirname + '/resources/views');
+
+
 app.get('/', (req, res) => {
-  res.send(`
-  <h1> HELLO WORLD </h1>
-  `)
+    res.render('home');
 })
 
 app.listen(port, () => {
